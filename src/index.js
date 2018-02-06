6d14978dbfaac28df33f3c9bacdd1004da0afe38
takefumi-yoshii <takefumi.yoshii@dena.com>
2018-02-06 14:18:17 +0900

    add bundler

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ const mittMiddleware = store => next => action => {
     m.on(type, handler)
     return () => m.off(type, handler)
   }
-  return {
+  return Object.assign({},
     ...store,
     dispatch,
     subscribeAction
-  }
+  )
 }
 
 export { mittMiddleware  }
