0257d9788eca62d4bdc9003bb75e63410358da8c
takefumi-yoshii <takefumi.yoshii@dena.com>
2018-03-23 01:19:24 +0900

    fix dispatch order

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const mittMiddleware = store => next => action => {
   const m = mitt()
   const store = next(action)
   const dispatch = action => {
+    store.dispatch(action)
     m.emit(action.type, action)
-    return store.dispatch(action)
   }
   const subscribeAction = (type, handler) => {
     if (typeof type !== 'string') throw new TypeError("First argument must be string of action.")
